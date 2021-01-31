503ae21d1d0d1d26117a3fe8aa016dbbcddb20b9
overun9299 <overun9299@163.com>
2021-01-31 16:57:54 +0800

    日常迭代

--- src/main/java/com/soap/entity/query/GoodsQuery.java
+++ src/main/java/com/soap/entity/query/GoodsQuery.java
@@ -1,6 +1,8 @@
 package com.soap.entity.query;
 
 import com.soap.utils.PageHelper;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * Created by ZhangPY on 2021/1/24
@@ -8,5 +10,7 @@ import com.soap.utils.PageHelper;
  * overun9299@163.com
  * Explain: GoodsQuery
  */
+@Data
+@Accessors(chain = true)
 public class GoodsQuery extends PageHelper {
 }
